b726306f3d33e6857b8771831ccd84edb6b1fd12
Lucas Vaz <lucasalvesvaz@hotmail.com>
2023-10-13 15:37:22 -0300

    feature: eureka server created

---
+++ ms-eureka-server/src/test/java/br/lucas/vaz/mseurekaserver/MsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.lucas.vaz.mseurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
