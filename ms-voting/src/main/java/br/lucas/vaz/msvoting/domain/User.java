a10887909e20e6b14d5e8830c24d14ae1582ae2c
Lucas Vaz <lucasalvesvaz@hotmail.com>
2023-10-17 15:59:03 -0300

    feature: feign clients and user and poll classes

---
+++ ms-voting/src/main/java/br/lucas/vaz/msvoting/domain/User.java
@@ -0,0 +1,12 @@
+package br.lucas.vaz.msvoting.domain;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+  private String cpf;
+
+  private String name;
+
+}
---
+++ ms-voting/src/main/java/br/lucas/vaz/msvoting/infra/feignClient/PollControllerClient.java
@@ -0,0 +1,13 @@
+package br.lucas.vaz.msvoting.infra.feignClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import br.lucas.vaz.msvoting.domain.Poll;
+
+@FeignClient(value = "ms-poll", path = "/polls")
+public interface PollControllerClient {
+  @GetMapping("/{id}")
+  Poll getById(@PathVariable("id") Long id);
+}
