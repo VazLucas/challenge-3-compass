fb0b2f617333f4d822ea7c0140ddab65e24158a1
Lucas Vaz <lucasalvesvaz@hotmail.com>
2023-10-19 16:45:52 -0300

    feature: find user and poll in vote repository

--- ms-voting/src/main/java/br/lucas/vaz/msvoting/infra/repository/VotesRepository.java
+++ ms-voting/src/main/java/br/lucas/vaz/msvoting/infra/repository/VotesRepository.java
@@ -1,12 +1,16 @@
 package br.lucas.vaz.msvoting.infra.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.lucas.vaz.msvoting.domain.Votes;
 
 public interface VotesRepository extends JpaRepository<Votes, Long> {
+
   @Override
   <S extends Votes> List<S> findAll(Example<S> example);
+
+  Optional<Votes> findByUserIdAndPollId(String userId, Long pollId);
 }
