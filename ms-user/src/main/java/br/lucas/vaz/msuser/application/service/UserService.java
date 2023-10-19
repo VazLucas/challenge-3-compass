7b36b5ce1b7d7b152df106ca0c4c3d2f2244ac3f
Lucas Vaz <lucasalvesvaz@hotmail.com>
2023-10-19 18:56:26 -0300

    refactor: moving code from controller to service

--- ms-user/src/main/java/br/lucas/vaz/msuser/application/service/UserService.java
+++ ms-user/src/main/java/br/lucas/vaz/msuser/application/service/UserService.java
@@ -3,8 +3,12 @@ package br.lucas.vaz.msuser.application.service;
 import java.util.Optional;
 
 import org.hibernate.validator.internal.constraintvalidators.hv.br.CPFValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.caelum.stella.format.Formatter;
+import br.lucas.vaz.msuser.application.exceptions.InvalidCpf;
+import br.lucas.vaz.msuser.application.exceptions.InvalidCpfFormat;
 import br.lucas.vaz.msuser.domain.User;
 import br.lucas.vaz.msuser.infra.repository.UserRepository;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,13 +20,40 @@ import lombok.RequiredArgsConstructor;
 
 public class UserService {
   private final UserRepository userRepository;
+  @Autowired
+  private Formatter formatter;
 
   @Transactional
   public User save(User user) {
-    return userRepository.save(user);
+
+    if (!formatter.isFormatted(user.getCpf())) {
+      try {
+        String cpfFormatted = formatter.format(user.getCpf());
+        user.setCpf(cpfFormatted);
+      } catch (Exception e) {
+        throw new InvalidCpfFormat();
+      }
+    }
+    if (!validator(user.getCpf())) {
+      throw new InvalidCpf();
+    }
+
+    Optional<User> userOptional = userRepository.findById(user.getCpf());
+
+    if (userOptional.isPresent()) {
+      throw new RuntimeException("User already registered");
+    }
+
+    User userCreated = user;
+    return userRepository.save(userCreated);
   }
 
   public Optional<User> get(String cpf) {
+    String cpfFormatted = formatter.format(cpf);
+
+    if (!validator(cpfFormatted)) {
+      throw new InvalidCpf();
+    }
     return userRepository.getByCpf(cpf);
   }
 
